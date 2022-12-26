d9e58bf51564d415c1516aeb8a2476d43dc2753a
anuj2801 <anujkirodia@gmail.com>
2022-12-27 02:34:47 +0530

    second commit

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,44 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const userSchema=new mongoose.Schema({
-        
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter a name"],
+  },
 
-})
+  avatar: {
+    public_id: String,
+    url: String,
+  },
 
-module.exports = mongoose.model('user', userSchema)
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: [true, "Email already exists"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minlength: [6, "Password must be at least of 6 characters"],
+    select: false,
+  },
+
+  followers:[
+    {
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"user",
+    },
+  ],
+
+  following:[
+    {
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"user",
+    },
+  ],
+
+
+});
+
+module.exports = mongoose.model("user", userSchema);
